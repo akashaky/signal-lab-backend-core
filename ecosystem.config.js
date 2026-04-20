9481e096fb6b48bd477a955c9c39d14dca84055f
Akash Pandey <akashakypandey@gmail.com>
2026-04-21 00:02:48 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "Main backend",
+      name: "signal-lab-backend-core",
       script: "web/index.js",
       instances: "1",
       exec_mode: "fork",
